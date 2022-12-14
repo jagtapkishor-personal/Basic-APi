64d8bc6d56ea10ceedc1e7137d62c5e37aac8856
Kishor <jagtapkishor104@gmail.com>
2022-12-14 16:03:09 +0530

    controller

---
+++ Routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+const controller = require("../Controller/user")
+
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, res, callback) => {
+            let type = req.params.type;
+            let path = "./assets/images";
+            callback(null, path)
+        },
+        filename: (req, file, callback) => {
+            callback(null, file.originalname, file.fieldname)
+        }
+    })
+});
+
+router.post("/adduser", upload.single("image"), controller.adduser);
+router.get("/getuser", controller.postuser)
+router.get("/getuserbyid/:id", controller.getUserById);
+router.get("/getuserByName/:name",controller.getUserByName)
+
+module.exports = router;
--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(cors());
-const multer = require("multer")
-const userModel = require("./Model/user")
+const userModel = require("./Routes/user")
+
+DB = process.env.Url.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.Url, { useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true },
     (err, result) => {
         if (err) {
             console.log(`problem to connet with mongodb`);
@@ -22,57 +23,10 @@ mongoose.connect(process.env.Url, { useNewUrlParser: true, useUnifiedTopology: t
         }
     })
 
-var upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, res, callback) => {
-            let type = req.params.type;
-            let path = "./assets/images";
-            callback(null, path)
-        },
-        filename: (req, file, callback) => {
-            callback(null, file.originalname, file.fieldname)
-        }
-    })
-});
+
+app.use("/userData", userModel)
 
 
-app.post("/user", upload.single("image"), async (req, res) => {
-    const userData = new userModel({
-        userName: req.body.userName,
-        image: req.file?.originalname,
-        path: req.file?.path
-    });
-    userData.save((err, result) => {
-        if (err) {
-            res.send({
-                message: "Something went wrong",
-            })
-        }
-        else if (result) {
-            res.send({
-                message: "User Saved",
-                data: result,
-            })
-        }
-    })
-
-});
-
-app.get("/user", async (req, res) => {
-    const result = await userModel.find();
-    console.log(result);
-    if (result.length > 0) {
-        res.send({
-            message: "All users",
-            data: result,
-        })
-    }
-    else {
-        res.send({
-            message: "No user Found"
-        })
-    }
-})
 
 app.get("/", (req, res) => {
     res.status(200).send("App Works")
